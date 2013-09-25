e8c66269dfafa3f920d58514a76e714ecc58883b
mitsuplit <mitsuplit@gmail.com>
2013-09-25 02:21:32 +0000

    Client authentication

--- server.py
+++ server.py
@@ -8,43 +8,60 @@ BUFFER_SIZE = 1024 # use a power of 2 number
 ID = 4235063100
 
 def handler(client_conn, client_addr):
-	print "Connected with ", client_addr
-	data = client_conn.recv(BUFFER_SIZE)
+	global ID
+
+	print "Connected with ", client_addr # Debug print
+	
+	# Client request: create stream
+	data = client_conn.recv(BUFFER_SIZE) # Debug print
 	print "Message from client: ", data
 	
-	res = "<?xml version=\'1.0\'?>" 
-	#change id
-	res += "<stream:stream xmlns=\'jabber:client\' xmlns:stream=\'http://etherx.jabber.org/streams\'"
-	res += " id=\'" + str(ID) + "\' from=\'127.0.0.1\' version=\'1.0\' xml:lang=\'en\'>"
-	res += "<stream:features>"
-	res += "<compression xmlns=\"http://jabber.org/features/compress\">"
-	res += "<method>zlib</method>"
-	res += "</compression>"
-	res += "<auth xmlns=\'http://jabber.org/features/iq-auth\'/>"
-	res += "</stream:features>"
+	# Server response: stream ID and available features
+	resStream = "<?xml version=\'1.0\'?>" 
+	resStream += "<stream:stream xmlns=\'jabber:client\' xmlns:stream=\'http://etherx.jabber.org/streams\'"
+	resStream += " id=\'" + str(ID) + "\' from=\'" + str(IP) + "\' version=\'1.0\' xml:lang=\'en\'>"
+	ID += 1
+	resStream += "<stream:features>"
+	resStream += "<compression xmlns=\"http://jabber.org/features/compress\">"
+	resStream += "<method>zlib</method>"
+	resStream += "</compression>"
+	resStream += "<auth xmlns=\'http://jabber.org/features/iq-auth\'/>"
+	resStream += "</stream:features>"	
+	client_conn.send(resStream)
 	
-	client_conn.send(res)
+	# Client request: Authentication Fields
+	data = client_conn.recv(BUFFER_SIZE)
+	iq = xmpp.protocol.Iq(node=data)
+	iqId = iq.getID()
+	iqUser = iq.getCDATA()
 	
+	# Server response: Authentication Fields
+	resIq = "<iq id=\'" + str(iqId) + "\' type=\'result\'><query xmlns=\'jabber:iq:auth\'><username/><password/><digest/><resource/></query></iq>"
+	client_conn.send(resIq)
+	
+	# Client response: Authentication information
+	data = client_conn.recv(BUFFER_SIZE)
+	iq = xmpp.protocol.Iq(node=data)
+	iqId = iq.getID()
+	userDigest = iq.getCDATA()
+	userDigest = userDigest.replace(iqUser,"")
+	userDigest = userDigest.replace("botty","")
+	print "User:", iqUser		# Debug print
+	print "Digest:", userDigest # Debug print
+	
+	# Server response: Authentication status
+	if 1:
+		authResponse = "<iq type=\'result\' id=\'" + str(iqId) + "'/>"
+		client_conn.send(authResponse)
+	else:
+		authResponse = "<iq type=\'result\' id=\'" + str(iqId) + "'><error code=\'401\' type=\'auth\'> <not-authorized xmlns=\'urn:ietf:params:xml:ns:xmpp-stanzas\'/> </error></iq>"
+		client_conn.send(authResponse)
+	
+	# Message handler	
 	while 1:
 		data = client_conn.recv(BUFFER_SIZE)
 		print "Message from client: ", data
-				
-		#con  = "<?xml version=\'1.0\'?><stream:stream xmlns=\"jabber:client\" to=\"127.0.0.1\" version=\"1.0\" xmlns:stream=\"http://etherx.jabber.org/streams\" >"
 		
-		#if data == con:
-		#	client_conn.send(res)
-		#	print "Con"
-		if data == "<iq type=\"get\" id=\"1\"><query xmlns=\"jabber:iq:auth\"><username>user1</username></query></iq>":
-			client_conn.send("<iq id=\'1\' type=\'result\'><query xmlns=\'jabber:iq:auth\'><username>username</username><password/><digest/><sequence>499</sequence><token>3D5A392D</token><resource/></query></iq>")
-		else: #data == "<iq type=\"set\" id=\"2\"><query xmlns=\"jabber:iq:auth\"><username /><digest>00f2dd21edafe827d8369a7e7b9c52aa2b09cc31</digest><sequence>499</sequence><token>3D5A392D</token><resource>xmpppy</resource></query></iq>"
-			client_conn.send("<iq id=\'2\' type=\'result\'/>")
-		#elif data == "<starttls xmlns=\"urn:ietf:params:xml:ns:xmpp-tls\"/>":
-		#	client_conn.send("<proceed xmlns=\"urn:ietf:params:xml:ns:xmpp-tls\"/>")
-		#	print "Data"
-		#else :
-		#	client_conn.send("<success xmlns=\'urn:ietf:params:xml:ns:xmpp-sasl\'/>")
-		#	print "Else"
-		#client_conn.send("res")
 		if data == "close\n":
 			client_conn.send("Bye..")
 			client_conn.close()
